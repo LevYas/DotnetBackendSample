28392f35f11d9e485b84f7a43d768958e1188aa5
Lev Yastrebov <levyas@outlook.com>
2020-05-04 17:43:33 +0300

    Added startup code

--- SugarCounter.Api/Program.cs
+++ SugarCounter.Api/Program.cs
@@ -1,26 +1,50 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SugarCounter.DataAccess.Db;
 
 namespace SugarCounter.Api
 {
     public class Program
     {
+        // Useful CLI commands:
+        //  Production env:
+        //      dotnet watch --project SugarCounter.Api/SugarCounter.Api.csproj run --configuration Release
+        //  Development env:
+        //      dotnet watch --project SugarCounter.Api/SugarCounter.Api.csproj run --launch-profile SugarCounter.Api.Dev
+        // Be sure that ./Properties/launchSettings.json contains properly configured profiles!
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+
+                try
+                {
+                    DbInitializer.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while initializing the database.");
+                    return;
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+                .ConfigureLogging(logging =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
--- SugarCounter.Api/Startup.cs
+++ SugarCounter.Api/Startup.cs
@@ -1,48 +1,48 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SugarCounter.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using SugarCounter.Api.Auth;
+using Microsoft.AspNetCore.Authentication;
+using SugarCounter.Api.Controllers.Food;
 
 namespace SugarCounter.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureDataAccess(Configuration.GetConnectionString("DefaultConnection"));
             services.AddControllers();
+            services.AddApiVersioning();
+
+            services.AddAuthentication(AuthHandler.AuthSchemeName)
+                .AddScheme<AuthenticationSchemeOptions, AuthHandler>(AuthHandler.AuthSchemeName, _ => { });
+
+            services.AddScoped(_ => new RequestContext());
+            services.AddHttpClient<INutritionDataProvider, NutritionixClient>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
+            else
+                app.UseExceptionHandler("/error");
 
             app.UseRouting();
 
+            app.UseMiddleware<RequestContextCapturer>();
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
